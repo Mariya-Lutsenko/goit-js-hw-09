2d1e58a497f7d0e555390edc02160949be8f20be
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2022-11-25 18:33:37 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,15 @@
-
-
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const DELAY = 1000;
 let timerId = null;
-stopBtn.setAttribute('disabled', false);
+stopBtn.disabled = true;
 
 startBtn.addEventListener('click', onClickStartBtn);
 stopBtn.addEventListener('click', onClickStopBtn);
 
 function onClickStartBtn () {
-    startBtn.setAttribute('disabled', false);
-    stopBtn.setAttribute('disabled', true);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
     timerId = setInterval(() =>{
 document.body.style.backgroundColor = getRandomHexColor();
     }, DELAY);
@@ -21,8 +19,8 @@ document.body.style.backgroundColor = getRandomHexColor();
 
 function onClickStopBtn () {
     clearInterval(timerId);
-    startBtn.setAttribute('disabled', true);
-    stopBtn.setAttribute('disabled', false);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
 };
 
 function getRandomHexColor() {
